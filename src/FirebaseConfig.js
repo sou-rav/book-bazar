cbcc61a130996e3414671ab2f80bf2f8c8e88ebb
Sourav <iSourav13@outlook.com>
2021-04-08 20:35:22 +0600

    final

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqJtsqDk_yfQ1aiy1y1o4-rqDcR0WJ16k",
+    authDomain: "book-shop-2f523.firebaseapp.com",
+    projectId: "book-shop-2f523",
+    storageBucket: "book-shop-2f523.appspot.com",
+    messagingSenderId: "964521135176",
+    appId: "1:964521135176:web:62a343c4ff1bae1ee51c7d"
+};
+
+export default firebaseConfig;
